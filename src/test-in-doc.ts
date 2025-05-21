ee918cc8facd0076b478004c71f4910e38c7c268
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-05-22 01:12:26 +0900

    [add] defn pattern

--- src/test-in-doc.ts
+++ src/test-in-doc.ts
@@ -57,3 +57,5 @@ const test_in_doc_loop0: Cion.Lisp<`(let [f (fn [r x] (if (>= 0 x) r (f (+ r 1) 
 
 // -- def
 const test_in_doc_def0: Cion.Lisp<`(+ ${typeof test_in_doc_loop0} 5)`> = '9'
+const test_in_doc_def1: Cion.Lisp<`(fn [i] (+ 1 i))`> = '(fn [i] (+ 1 i))'
+const test_in_doc_def2: Cion.Lisp<`(${typeof test_in_doc_def1} ${typeof test_in_doc_def0})`> = '10'
