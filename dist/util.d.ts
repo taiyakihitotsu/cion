393a08e9d4f4ef1e79a839505ad76761e85c231e
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-05-25 15:40:37 +0900

    [fix] Equal

--- dist/util.d.ts
+++ dist/util.d.ts
@@ -1,7 +1,4 @@
 declare namespace Util {
-    type _Eq<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
-    type _Equal<X, Y> = X extends [infer XH, ...infer XR] ? Y extends [infer YH, ...infer YR] ? Equal<XH, YH> | Equal<XR, YR> : false : _Eq<X, Y>;
-    export type Equal<X, Y> = _Equal<X, Y> extends true ? true : false;
-    export {};
+    type Equal<X, Y> = (<T>() => T extends X & T | T ? 1 : 2) extends (<T>() => T extends Y & T | T ? 1 : 2) ? true : false;
 }
 export default Util;
--- src/util.ts
+++ src/util.ts
@@ -1,35 +1,8 @@
-// NOTE
-// ---------------------------------
-// type Equal<X, Y> = 
-//   (<T>() => T extends X ? 1 : 2) extends
-//   (<T>() => T extends Y ? 1 : 2) ? true : false
-// 
-// > https://github.com/microsoft/TypeScript/issues/27024
-// ---------------------------------
-// const eeeee0: Bit.BitLen<'3999'>  = [[[[null]]]]
-// const eeeee1: Bit.BitLen<'11111'> = [[[[[null]]]]]
-// const eeeee2: Equal<Bit.BitLen<'3999'>, Bit.BitLen<'11111'>> = true // => it should be false but the above definition returns 'true wrongly.
-// ---------------------------------
-// And they are able to be passed unintentionaly.
-//
-// const fffff0: typeof eeeee0 = eeeee0
-// const fffff1: typeof eeeee1 = fffff0
-
 namespace Util {
 
-type _Eq<X, Y> =
-  (<T>() => T extends X ? 1 : 2) extends
-  (<T>() => T extends Y ? 1 : 2) ? true : false;
-
-type _Equal<X,Y> =
-  X extends [infer XH, ...infer XR]
-    ? Y extends [infer YH, ...infer YR]
-        ? Equal<XH, YH> | Equal<XR, YR>
-        : false
-    : _Eq<X,Y>
-
-export type Equal<X,Y> = 
-  _Equal<X,Y> extends true ? true : false
+export type Equal<X, Y> =
+  (<T>() => T extends X & T | T ? 1 : 2) extends
+  (<T>() => T extends Y & T | T ? 1 : 2) ? true : false;
 }
 
 export default Util; 
