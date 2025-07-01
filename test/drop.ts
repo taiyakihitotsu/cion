5077df144de1c10b4bbe2d90bbe3a3709697013c
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-07-01 13:07:29 +0900

    [test] `drop`, `take`

--- test/drop.ts
+++ test/drop.ts
@@ -7,3 +7,8 @@ const maintest0_drop_0: Cion.RawLisp<'(drop 9 [0 1 2 3 4 5])'> = ['vec']
 const maintest0_drop_1: Cion.RawLisp<'(drop 2 [0 1 2 3 4 5])'> = ['vec', ['prim', '0000000000000010'], ['prim', '0000000000000011'], ['prim', '0000000000000100'], ['prim', '0000000000000101']]
 const maintest0_drop_2: Cion.RawLisp<'(drop 0 [0 1 2 3 4 5])'> = ['vec', ['prim', '0000000000000000'], ['prim', '0000000000000001'], ['prim', '0000000000000010'], ['prim', '0000000000000011'], ['prim', '0000000000000100'], ['prim', '0000000000000101']]
 
+
+const drop_test0 : Cion.Lisp<`(drop 0 [0 1 2 3 4 5 6])`> = '[0 1 2 3 4 5 6]'
+const drop_test1 : Cion.Lisp<`(drop 2 [0 1 2 3 4 5 6])`> = '[2 3 4 5 6]'
+const drop_test : Cion.Lisp<`(drop 9 [0 1 2 3 4 5 6])`> = '[]'
+
