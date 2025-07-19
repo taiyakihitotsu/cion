e2d1e90fd5b7d9dd208592f6500ccc6d30f560bd
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-07-20 02:27:13 +0900

    [fix] script

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-#!/bash/sh
+#!/bin/bash
 
-npm install && git submodule update --init --recursive && cd submodules/condsmith && npm install
+npm install && git submodule add -f -b main git@github.com:taiyakihitotsu/condscript.git submodules/scripts && git submodule add -f -b master git@github.com:taiyakihitotsu/condsmith.git submodules/condsmith && git submodule sync --recursive && git submodule init && git submodule update --init --recursive && cd submodules/condsmith && npm install
